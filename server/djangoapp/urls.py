c255155b4924572200317264c1296a7e618ec001
Pardeep On Windows <brar68099@gmail.com>
2023-02-02 06:10:43 -0500

    authentication complete

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,15 +10,18 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-    path(route='aboutus', view=views.about, name='about'),
+    path(route='aboutus/', view=views.about, name='about'),
     # path for contact us view
-    path(route='contactus', view=views.contact, name='contact'),
+    path(route='contactus/', view=views.contact, name='contact'),
 
     # path for registration
+    path('registration/', views.registration_request, name='registration'),
 
     # path for login
+    path(route='login/', view=views.login_request, name='login'),
 
     # path for logout
+    path(route='logout/', view=views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
